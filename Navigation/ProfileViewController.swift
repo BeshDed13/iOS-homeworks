e6221602da8546152c376056401de32dc59b3666
Dmitriy Ilinskiy <hardinc0695@gmail.com>
2025-11-08 20:50:09 +0300

    task completed

---
+++ Navigation/ProfileViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Дмитрий Ильинский on 08.11.2025.
+//
+
+import Foundation
+import UIKit
+
+class ProfileViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray
+    }
+}
