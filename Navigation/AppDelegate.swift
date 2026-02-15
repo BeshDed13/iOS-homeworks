527f0a32c270546ee685d4071d26fa6e5b69f714
Dmitriy Ilinskiy <hardinc0695@gmail.com>
2026-02-15 10:46:59 +0300

    Task 1 completed

--- Navigation/AppDelegate.swift
+++ Navigation/AppDelegate.swift
@@ -11,8 +11,28 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     var coordinator: TabBarCoordinator?
     
+    var appConfigutation: AppConfiguration!
+    
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let urls = [
+            URL(string: "https://swapi.dev/api/people/8/")!,
+            URL(string: "https://swapi.dev/api/starships/3/")!,
+            URL(string: "https://swapi.dev/api/planets/5/")!
+        ]
+        
+        let randIndex = Int.random(in: 0..<urls.count)
+        
+        switch randIndex {
+        case 0:
+            appConfigutation = .people(urls[0])
+        case 1:
+            appConfigutation = .starships(urls[1])
+        default:
+            appConfigutation = .planets(urls[2])
+        }
+        
+        NetworkService.request(for: appConfigutation)
         
         // create tab bar with feed and profile items
         let window = UIWindow(frame: UIScreen.main.bounds)
